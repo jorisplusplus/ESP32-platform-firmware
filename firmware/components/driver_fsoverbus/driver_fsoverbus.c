f55d6c0c08a9395822e9027bcc3811679b4b75e4
Joris Witteman <joriswitteman@hotmail.com>
2020-10-28 20:22:45 +0100

    FSoverUart implimentation from cz20 now with support for other hardware layers

---
+++ firmware/components/driver_fsoverbus/driver_fsoverbus.c
@@ -0,0 +1,246 @@
+#include <sdkconfig.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <esp_err.h>
+#include <esp_log.h>
+#include <driver/uart.h>
+#include <driver/gpio.h>
+#include <esp_vfs.h>
+#include <dirent.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/semphr.h"
+#include "freertos/queue.h"
+#include "freertos/timers.h"
+
+#include "include/driver_fsoverbus.h"
+#include "include/filefunctions.h"
+#include "include/packetutils.h"
+#include "include/specialfunctions.h"
+#include "include/fsob_backend.h"
+
+//TEMP
+#include <driver/spi_master.h>
+
+
+#ifdef CONFIG_DRIVER_FSOVERBUS_ENABLE
+
+void vTimeoutFunction( TimerHandle_t xTimer );
+
+#define TAG "fsoverbus"
+
+TimerHandle_t timeout;
+RingbufHandle_t buf_handle[2];
+
+
+uint8_t command_in[1024];
+
+
+#define min(a,b) (((a) < (b)) ? (a) : (b))
+
+//Function lookup tables
+
+int (*specialfunction[])(uint8_t *data, uint16_t command, uint32_t message_id, uint32_t size, uint32_t received, uint32_t length) = {execfile, heartbeat, pythonstdin};
+int specialfunction_size = 3;
+                         //                                                                                  4096    4097      4098       4099     4100      4101    4102
+int (*filefunction[])(uint8_t *data, uint16_t command, uint32_t message_id, uint32_t size, uint32_t received, uint32_t length) = {getdir, readfile, writefile, delfile, duplfile, mvfile, makedir};
+int filefunction_size = 7;
+
+void handleFSCommand(uint8_t *data, uint16_t command, uint32_t message_id, uint32_t size, uint32_t received, uint32_t length) {
+    static uint32_t write_pos;
+    if(received == length) { //First packet
+        write_pos = 0;
+        //ESP_LOGI(TAG, "clear");
+    }
+    if(length > 0) {
+        memcpy(&command_in[write_pos], data, length);
+        write_pos += length;
+    }
+
+    int return_val = 0;
+    if(command < 4096) {
+        if(command < specialfunction_size) {
+            return_val = specialfunction[command](command_in, command, message_id, size, received, length);
+        }
+    } else if(command < 8192) {
+        if((command-4096) < filefunction_size) {
+            return_val = filefunction[command-4096](command_in, command, message_id, size, received, length);
+        }
+    }
+    //ESP_LOGI(TAG, "%d", return_val);
+    if(return_val) {    //Function has indicated that payload should write at start of buffer.
+        write_pos = 0;
+    }
+}
+
+void vTimeoutFunction( TimerHandle_t xTimer ) {
+    ESP_LOGI(TAG, "Saw no message for 1s assuming task crashed. Resetting...");
+    fsob_reset();
+}
+
+static ssize_t bypass_write(int fd, const void * data, size_t size)
+{  
+    if(fd > 1) {
+        ESP_LOGW(TAG, "Wrong fd %d", fd);
+        return 0;
+    }
+    return xRingbufferSend(buf_handle[fd], data, size, pdMS_TO_TICKS(1))*size;
+}
+
+static int bypass_open(const char * path, int flags, int mode) {
+    if(strcmp("/1", path) == 0) {
+        return 0;
+    } else if(strcmp("/2", path) == 0) {
+        return 1;
+    }
+    ESP_LOGW(TAG, "wrond mountpoint %s", path);
+    return 2;
+}
+
+static int bypass_fstat(int fd, struct stat * st) {
+    return 0;
+}
+
+static int bypass_close(int fd) {
+    return 0;
+}
+
+static ssize_t bypass_read(int fd, void* data, size_t size) {
+        //Receive data from byte buffer
+    if(fd > 1) {
+        ESP_LOGW(TAG, "Wrong fd %d", fd);
+        return 0;
+    }
+    size_t item_size;
+    if(fd == 1) size = 1;   //Bit of an hack. Read seem to always be of length 128, this forces stdin of mp to only read 1ch
+    char *item = (char *)xRingbufferReceiveUpTo(buf_handle[fd], &item_size, pdMS_TO_TICKS(1), size);
+    
+    //Check received data
+    if (item != NULL) {
+        //Print item
+        memcpy(data, item, item_size);
+        //Return Item
+        vRingbufferReturnItem(buf_handle[fd], (void *)item);
+        return item_size;
+    } else {
+        //Failed to receive item
+        return 0;
+    }
+}
+
+esp_vfs_t myfs = {
+.flags = ESP_VFS_FLAG_DEFAULT,
+.write = &bypass_write,
+.open = &bypass_open,
+.fstat = &bypass_fstat,
+.close = &bypass_close,
+.read = &bypass_read,
+};
+
+void fsob_stop_timeout() {
+     xTimerStop(timeout, 1);
+}
+
+void fsob_start_timeout() {
+    xTimerStart(timeout, 1);
+}
+
+//Temp
+static const spi_bus_config_t buscfg = {
+		.mosi_io_num     = 23,
+		.miso_io_num     = 35,
+		.sclk_io_num     = 18,
+		.max_transfer_sz = 19
+	};
+
+spi_device_handle_t handle;
+
+spi_device_interface_config_t devcfg={
+        .command_bits=0,
+        .address_bits=0,
+        .dummy_bits=0,
+        .clock_speed_hz=1000000,
+        .duty_cycle_pos=128,
+        .mode=0,
+        .spics_io_num=19,
+        .queue_size=3
+    };
+
+void process_miso(uint8_t *data) {
+
+}
+
+void stm32_task() {
+    for(;;) {
+        vTaskDelay(1);
+        if(!gpio_get_level(0)) {
+            uint8_t mosi[18] = {0};
+            uint8_t miso[18] = {0};
+            spi_transaction_t t;
+            t.length = 18*8;
+            t.tx_buffer = mosi;
+            t.rx_buffer = miso;
+            spi_device_transmit(handle, &t);
+            process_miso(&miso[9]);
+        }
+    }
+}
+
+void spi_init() {
+    esp_err_t res = spi_bus_initialize(VSPI_HOST, &buscfg, 1);
+    gpio_set_direction(19, GPIO_MODE_OUTPUT);
+    spi_bus_add_device(VSPI_HOST, &devcfg, &handle);
+    xTaskCreatePinnedToCore(stm32_task, "fsoverbus_spi", 16000, NULL, 100, NULL, 0);
+
+}
+
+void fsob_write_bytes(const char *src, size_t size) {
+    while (size > 0) {
+        uint8_t mosi[18] = {0};
+        uint8_t miso[18] = {0};
+        mosi[1] = 0xFF;
+        mosi[0] = min(size, 6);
+        memcpy(src, &mosi[2], mosi[0]);
+        src += mosi[0]; //Increment src pointer with bytes copied
+        spi_transaction_t t;
+        t.length = 18*8;
+        t.tx_buffer = mosi;
+        t.rx_buffer = miso;
+        spi_device_transmit(handle, &t);
+        process_miso(&miso[9]);
+        size = size - min(size, 6);
+    }
+}
+
+
+
+//Temp end
+esp_err_t driver_fsoverbus_init(void) { 
+
+    buf_handle[0] = xRingbufferCreate(2048, RINGBUF_TYPE_BYTEBUF);
+    buf_handle[1] = xRingbufferCreate(2048, RINGBUF_TYPE_BYTEBUF);
+
+    ESP_ERROR_CHECK(esp_vfs_register("/dev/fsou", &myfs, NULL));
+    
+    fsob_init();
+    spi_init();
+
+    ESP_LOGI(TAG, "fs over bus registered.");
+    
+    timeout = xTimerCreate("FSoverBUS_timeout", 100, false, 0, vTimeoutFunction);
+    return ESP_OK;
+} 
+
+#if CONFIG_DRIVER_FSOVERBUS_NOBACKEND_HELPER
+
+#else
+
+#endif
+
+#else
+esp_err_t driver_fsoverbus_init(void) { return ESP_OK; } //Dummy function.
+#endif
